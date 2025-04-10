978e6ce3fa3437bf1094057bb2b51da0a7aa7e8b
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-04-10 04:02:18 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomNumber (MyArray) {
+  return Math.floor(Math.random() * MyArray.length)
+}
+
+let randomExcuse = who[getRandomNumber(who)] + " " + action[getRandomNumber(action)] + " " + what[getRandomNumber(what)] + " " + when[getRandomNumber(when)];
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector('#excuse').innerHTML = randomExcuse;
+
 };
